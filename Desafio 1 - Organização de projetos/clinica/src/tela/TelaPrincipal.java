00ce9601823da31366f1bfb37189fac859c361eb
Antonio Bigas <100382731+antoniobigas@users.noreply.github.com>
2022-11-10 22:39:35 -0300

    adicionei os exercicios e criei o projeto
    primeiro projeto do curso.

---
+++ Desafio 1 - Organização de projetos/clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,10 @@
+package tela;
+
+public class TelaPrincipal {
+	
+	// tudo começa a partir de um main!! sempre começar com letra minúscula
+	//utilizar main + ctrl espaço
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Desafio 1 - Organização de projetos/faculdade/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author antonio
+ *
+ */
+module faculdade {
+}
